f5bcdb9d53a735338f90028ec71b037385e9e252
kyao <kzmeyao@gmail.com>
2015-02-20 00:15:09 -0800

    add active state animations

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 (function () {
-  var Router = ReactRouter, DefaultRoute = Router.DefaultRoute, Link = Router.Link, Route = Router.Route, RouteHandler = Router.RouteHandler;
+  var Router = ReactRouter,
+      DefaultRoute = Router.DefaultRoute,
+      Link = Router.Link,
+      Route = Router.Route,
+      RouteHandler = Router.RouteHandler;
+
+  var activeStateController = {
+    assignNewState: function (to) {
+      var icon = document.getElementsByClassName("icon-" + to)[0];
+      var active = document.getElementById("nav-active");
+      if (active != null) {
+        var dist = active.offsetTop - icon.offsetTop + 22;
+        TweenMax.to(active, .3, {autoAlpha: .5, display: 'block'});
+        TweenMax.to(active, .3, {y : -dist});
+      }
+    },
+    restoreState: function () {
+      var icon = document.getElementsByClassName("nav-logo")[0];
+      var active = document.getElementById("nav-active");
+      if (active != null) {
+        var dist = active.offsetTop - icon.offsetTop + 22;
+        TweenMax.to(active, .3, {autoAlpha: 0, display: 'none'});
+        TweenMax.to(active, .3, {y : dist});
+      }
+    }
+  };
 
   var App = React.createClass({
     mixins: [{
       insertLogo: function () {
         var desktopNode = this.refs.logoDesktop.getDOMNode();
         var mobileNode = this.refs.logoMobile.getDOMNode();
-        desktopNode.innerHTML = mobileNode.innerHTML = '<svg height="40" width="40"><circle cx="20" cy="20" r="20" fill="rgba(255,255,255,0.25)" /><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="jelly" x="10" y="10" width="20" height="20" viewBox="0 0 8 8" enable-background="new 0 0 8 8" fill="#FFFFFF" xml:space="preserve"><path d="M4 0.2c-1.9 0-3.5 1.5-3.5 3.4 0 1.9 3.5 1.7 3.5 1.7s3.5 0.2 3.5-1.7C7.5 1.7 5.9 0.2 4 0.2z"/><g class="tentacles"><rect x="3.7" y="4.9" width="0.6" height="2.7"/><rect x="5.4" y="4.8" width="0.6" height="2.3"/><rect x="2.1" y="4.8" width="0.6" height="2.3"/></g></svg></svg>';
+        desktopNode.innerHTML = '<svg height="40" width="40"><circle cx="20" cy="20" r="20" fill="rgba(255,255,255,0.25)" /><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="jelly" x="10" y="10" width="20" height="20" viewBox="0 0 8 8" enable-background="new 0 0 8 8" fill="#FFFFFF" xml:space="preserve"><path d="M4 0.2c-1.9 0-3.5 1.5-3.5 3.4 0 1.9 3.5 1.7 3.5 1.7s3.5 0.2 3.5-1.7C7.5 1.7 5.9 0.2 4 0.2z"/><g class="tentacles"><rect x="3.7" y="4.9" width="0.6" height="2.7"/><rect x="5.4" y="4.8" width="0.6" height="2.3"/><rect x="2.1" y="4.8" width="0.6" height="2.3"/></g></svg></svg>';
+        mobileNode.innerHTML = '<svg height="40" width="40"><circle cx="20" cy="20" r="20" fill="rgba(255,255,255,0.25)" /><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="jelly" x="10" y="10" width="20" height="20" viewBox="0 0 8 8" enable-background="new 0 0 8 8" fill="#FFFFFF" xml:space="preserve"><path d="M4 0.2c-1.9 0-3.5 1.5-3.5 3.4 0 1.9 3.5 1.7 3.5 1.7s3.5 0.2 3.5-1.7C7.5 1.7 5.9 0.2 4 0.2z"/><g class="tentacles"><rect x="3.7" y="4.9" width="0.6" height="2.7"/><rect x="5.4" y="4.8" width="0.6" height="2.3"/><rect x="2.1" y="4.8" width="0.6" height="2.3"/></g></svg></svg><div id="nav-active"><svg height="40" width="40"><circle cx="20" cy="20" r="20" fill="#569ea0" /></svg></div>';
       }
-    }],
+    }, activeStateController],
     render: function () {
       return (
         <div>
@@ -27,6 +53,7 @@
           </nav>
           <nav className="nav-mobile">
             <div className="nav-overlay">
+              <div className="state-placeholder"></div>
               <div className="nav-wrapper">
                 <Link to="/" className="nav-logo" ref="logoMobile"></Link>
                 <div className="icon"><Link to="write" className="icon-keyboard"></Link></div>
@@ -46,6 +73,7 @@
   });
 
   var Home = React.createClass({
+    mixins: [activeStateController],
     render: function () {
       var text = "... wait. You came for the jellies, didn't you?";
       var buttonText = "Guilty as charged";
@@ -61,30 +89,45 @@
           <article className="segment"></article>
         </div>
       );
-    }
-  });
-
-  var Shoot = React.createClass({
-    render: function () {
-      return (
-        <div></div>
-      )
+    },
+    componentDidMount: function () {
+      this.restoreState();
     }
   });
 
   var Write = React.createClass({
+    mixins: [activeStateController],
     render: function () {
       return (
         <div></div>
       )
+    },
+    componentDidMount: function () {
+      this.assignNewState('keyboard');
+    }
+  });
+
+  var Shoot = React.createClass({
+    mixins: [activeStateController],
+    render: function () {
+      return (
+        <div></div>
+      )
+    },
+    componentDidMount: function () {
+      this.assignNewState('camera');
     }
   });
 
   var Greet = React.createClass({
+    mixins: [activeStateController],
     render: function () {
       return (
         <div></div>
       )
+    },
+    componentDidMount: function () {
+      this.assignNewState('profile');
     }
   });
 
