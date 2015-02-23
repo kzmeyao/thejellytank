221d593c50d912d2d5b9e2754dfab173220d4f3c
kyao <kzmeyao@gmail.com>
2015-02-22 21:57:56 -0800

    fix mobile address bar scrolling transition gap

--- js/main.js
+++ js/main.js
@@ -75,6 +75,7 @@
           setTimeout(function(){that.assignNewState(match[1]);}, 1000);
         }
       }
+      document.getElementsByTagName("nav")[0].style.height = window.innerHeight + 60 + "px";
       var particles = new Particles("nav");
       for (var i = 0; i < 50; i++) {
         var particle = document.createElement('div');
